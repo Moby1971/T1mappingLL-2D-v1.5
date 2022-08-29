a03311847f63934f7ea45aaee0c4610c26427ba8
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2022-08-29 11:17:46 +0200

    Maintenance

--- registration/register_images.m
+++ registration/register_images.m
@@ -1,4 +1,4 @@
-function imagesOut = register_images(app,imagesIn)
+function imagesOut = register_images(app,imagesIn,mask)
 
 %------------------------------------------------------------
 % Image registration for inversion recovery T1 mapping tool
@@ -11,11 +11,16 @@ function imagesOut = register_images(app,imagesIn)
 %
 %------------------------------------------------------------
 
-
+% pp.mask = ones(app.dimx,app.dimy,app.ns,app.nd);
 
 [nEchoes,~,~,nSlices,nDynamics] = size(imagesIn);
 
+mask = permute(mask,[5 1 2 3 4]);
+imagesIn = imagesIn.*mask;
+
+
 [optimizer, metric] = imregconfig('multimodal');
+
 
 loops = nDynamics*nSlices*(nEchoes-1);
 cnt = 0;
@@ -29,10 +34,10 @@ for slice = 1:nSlices
             image0 = squeeze(imagesIn(1,:,:,slice,dynamic));
             image1 = squeeze(imagesIn(echo,:,:,slice,dynamic));
 
-            max0 = max(image0(:));
-            max1 = max(image1(:));
+            max0 = median(image0(:));
+            max1 = median(image1(:));
 
-            [image2] = imregister(image1/max1,image0/max0,'rigid',optimizer, metric,'DisplayOptimization',0);
+            image2 = imregister(image1/max1,image0/max0,'affine',optimizer, metric, 'DisplayOptimization', 0);
 
             imagesIn(echo,:,:,dynamic) = image2*max1;
 
